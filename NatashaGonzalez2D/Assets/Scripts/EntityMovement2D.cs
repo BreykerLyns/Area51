a2dd3839165ddca9ea2e90c85803b895f0cd289c
Natasha <darkestmoonlights6@gmail.com>
2018-09-25 11:25:14 -0500

    Edit

---
+++ NatashaGonzalez2D/Assets/Scripts/EntityMovement2D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntityMovement2D : MonoBehaviour {
+
+    public float speed = 1f;
+    public Vector2 direction;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(direction * speed * Time.deltaTime);
+		
+	}
+}
---
+++ NatashaGonzalez2D/Assets/Scripts/MovimientoControlado2D.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoControlado2D : MonoBehaviour {
+    
+    public float speed;
+    public float limitRight, limitLeft;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && transform.position.x > limitLeft)
+        {
+            transform.Translate(Vector3.left);
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && transform.position.x < limitRight)
+        {
+            transform.Translate(Vector3.right);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            transform.Translate(Vector3.down);
+        }
+        else if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(Vector3.up);
+        }
+	}
+}
