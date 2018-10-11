c689ae0dc578528d0ad82e291f6cd8cb9dd2d356
BreykerLyns <darkestmoonlights6@gmail.com>
2018-10-11 13:15:24 -0500

    Agg

---
+++ NatashaGonzalez2D/Assets/TopDownProject/Scripts/Movement/Physics/EntityPhysicsMov2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class EntityPhysicsMov2D : MonoBehaviour {
+
+    public float speed = 1f;
+    public Vector2 direction;
+    public Vector3 movement { get { return direction * speed * Time.fixedDeltaTime; } }
+
+    public Rigidbody2D rb2D;
+
+    void Reset() {
+        rb2D = GetComponent<Rigidbody2D> ();
+        rb2D.gravityScale = 0;
+    }
+	
+    protected virtual void FixedUpdate() {
+        //transform.Translate(movement);
+        rb2D.MovePosition(transform.position + movement);
+
+	}
+}
---
+++ NatashaGonzalez2D/Assets/TopDownProject/Scripts/Movement/PingPongMovement2D.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PingPongMovement2D : EntityMovement2D
+{
+
+    public float distance = 1;
+    private float currentDistance;
+
+    public float totalMovement { get { return currentDistance + movement.magnitude;}}
+	// Use this for initialization
+	void Start () {
+        
+	}
+
+    protected override void Update() {
+        currentDistance += movement.magnitude;
+        //Logica PingPong
+        if (currentDistance >= distance) {
+            float delta = totalMovement - distance;
+            transform.Translate(direction.normalized * delta);
+            direction *= -1;
+            currentDistance = 0;
+        } else {
+            base.Update();
+        }
+    }
+}
