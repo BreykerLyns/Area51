6a451e7ed700076b4d3ccb6af8d6febd6de311f5
BreykerLyns <darkestmoonlights6@gmail.com>
2018-11-08 13:06:26 -0500

    dfgfh

---
+++ NatashaGonzalez3D/Assets/Scripts/CamBehaviour.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamBehaviour : MonoBehaviour {
+
+    public struct TargetData {
+
+        public Transform target;
+        public Vector3 focusDistance;
+        public Vector3 followDistance;
+        public float minFollowSpeed;
+
+        public void SetFromVariables (Transform target, Vector3 focus, Vector3 follow, float minSpeed) {
+            this.target = target;
+            focusDistance = focus;
+            followDistance = follow;
+            minFollowSpeed = minSpeed;
+            
+        }
+    }
+
+    public Transform target;
+    public Vector3 focusDistance;
+    Vector3 focusPoint{ get { return target.position + GetRelativePos(target, focusDistance); }}
+    public Vector3 followDistance;
+    Vector3 followPoint { get { return target.position + GetRelativePos(target, followDistance); }}
+    public float minFollowSpeed = 1f;
+    public float followDistanceDelta { get { return Vector3.Distance(transform.position, followPoint); }}
+
+    public TargetData defaultData;
+
+	// Use this for initialization
+
+	void Start () {
+
+        defaultData.SetFromVariables(target, focusDistance, followDistance, minFollowSpeed);
+	}
+
+    // Update is called once per frame
+    void LateUpdate() {
+        transform.position = Vector3.MoveTowards (transform.position, followPoint, (minFollowSpeed + followDistanceDelta) * Time.deltaTime);
+        transform.LookAt(focusPoint);
+    }
+
+    Vector3 GetRelativePos (Transform relativeTarget, Vector3 distace){
+        Vector3 relativePos;
+        relativePos = target.right * distace.x;
+        relativePos += relativeTarget.up * distace.y;
+        relativePos += relativeTarget.forward * distace.z;
+
+        return relativePos;
+    }
+
+    public void Reposition(Vector3 updatedPosition) {
+        transform.position = updatedPosition;
+    }
+
+	void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(followPoint, focusPoint);
+        Gizmos.DrawWireSphere(followPoint, 0.25f);
+        Gizmos.color = Color.white;
+        Gizmos.DrawSphere(focusPoint, 0.15f);
+    }
+}
--- NatashaGonzalez3D/Assets/Scripts/CharacterBaseMov3D.cs
+++ NatashaGonzalez3D/Assets/Scripts/CharacterBaseMov3D.cs
@@ -2,44 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterBaseMov3D : MonoBehaviour
-{
+public class CharacterBaseMov3D : MonoBehaviour {
+
+    public struct CharTransformData {
+        public Vector3 position;
+        public Quaternion rotation;
+    }
 
     Rigidbody rigBod;
     Vector3 movement;
-    Vector3 RespawnPos;
+
+    CharTransformData respawnData;
 
     public float speed = 5f;
+    public float angSpeed = 25f;
     public float jumpForce = 10f;
 
-    // Use this for initialization
-    void Start()
-    {
+	// Use this for initialization
+	void Start () {
         rigBod = GetComponent<Rigidbody>();
-    }
+        respawnData.position = transform.position;
+        respawnData.rotation = transform.rotation;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
+	}
+
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
             rigBod.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
         }
-    }
+	}
 
-
-    void FixedUpdate()
-    {
-        movement.z = Input.GetAxis("Vertical");
+	void FixedUpdate () {
+        movement = transform.forward * Input.GetAxis("Vertical");
         rigBod.MovePosition(rigBod.position + (movement * speed * Time.fixedDeltaTime));
-    }
+        Quaternion rotation = Quaternion.Euler(Vector3.up * Input.GetAxis("Horizontal") * angSpeed * Time.fixedDeltaTime);
+        rigBod.MoveRotation(rotation * rigBod.rotation);
+	}
 
-    void Respawn(){
+    void Respawn () {
         rigBod.velocity = Vector3.zero;
+        rigBod.MovePosition(respawnData.position);
+        CamBehaviour playerCam = Camera.main.GetComponent<CamBehaviour>();
+        playerCam.Reposition(respawnData.position + playerCam.followDistance);
     }
 
-    void OnTriggerExit(Collider other){
-        if (other.CompareTag("GameArea")){
+	void OnTriggerExit (Collider other) {
+        if (other.CompareTag("GameArea")) {
             Respawn();
         }
-    }
+	}
+	void OnDrawGizmos () {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, transform.forward);
+	}
 }
